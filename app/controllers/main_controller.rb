42b223e3b4a091f32d213bbe4ccc8a370d2b52d0
agedob <agedob888@gmail.com>
2018-04-26 08:43:08 -0700

    testing

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,17 @@
+class MainController < ApplicationController
+  def index
+  render layout: "index"
+  end
+
+  def single
+  end
+
+  def edit
+  end
+
+  def dashboard
+  end
+
+  def new
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,42 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @error = User.create(form_param).errors.full_messages
+      if @error.length > 0
+        flash[:error] = @error
+        redirect_to root_url
+      else
+        session[:user_id] = User.find_by_email(form_param[:email]).id
+        redirect_to '/main/dashboard'
+      end
+    end
+
+  def login
+      # Log User In
+      if !(User.find_by_email(params[:email]).try(:authenticate, params[:password])).nil?
+        session[:user_id] = User.find_by(email: params[:email]).id
+        puts session[:user_id]
+        redirect_to '/main/dashboard'
+      else
+        redirect_to '/'
+      end
+        # if 
+          # save user id to session
+          # redirect to users profile page
+      # if authenticate false
+          # add an error message -> flash[:errors] = ["Invalid"]
+          # redirect to login page
+  end
+  def destroy
+    reset_session
+    redirect_to '/'
+  end
+
+  private
+    def form_param
+      params.require(:form).permit(:first_name,:last_name, :email, :password, :password_confirmation)
+    end
+
+end
